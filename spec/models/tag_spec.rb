149c9ae84073a028a5c73673df854402ab1837e4
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-25 18:26:26 -0400

    add test for tags that are too long

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Tag, type: :model do
     tag = Tag.new(name: 'fail')
     expect(tag).to be_valid
   end
+
+  it 'is invalid without a name' do
+    tag = Tag.new()
+    tag.valid?
+    expect(tag.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid if longer than 16 characters' do
+    tag = Tag.new(name: 'failure to launch')
+    tag.valid?
+    expect(tag.errors[:name]).to include("is too long (maximum is 16 characters)")
+  end
 end
