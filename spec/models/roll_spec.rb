a2da0f9e2dd7f09b07d7f7289e6ccb66e531eabd
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-25 17:59:27 -0400

    change 'Rick Roll' test roll titles to 'Bang' to reflect actual content of video

--- spec/models/roll_spec.rb
+++ spec/models/roll_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Roll, type: :model do
   describe "validity:" do
     it 'is valid when it has a title, start time, end time, and youtube uri' do
-      roll = Roll.new(title: 'Rick Roll', start_time: 19, end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
+      roll = Roll.new(title: 'Bang', start_time: 19, end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
       expect(roll).to be_valid
     end
 
@@ -14,37 +14,37 @@ RSpec.describe Roll, type: :model do
     end
 
     it 'is not valid when the title is over 50 characters long' do
-      roll = Roll.new(title: 'Rick rolls his friends. They are startled, but decide to maintain their friendship. After all, Rick is a good guy.', start_time: 19, end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
+      roll = Roll.new(title: 'Bangs his friends. They are startled, but decide to maintain their friendship. After all, Rick is a good guy.', start_time: 19, end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
       roll.valid?
       expect(roll.errors[:title]).to include("is too long (maximum is 50 characters)")
     end
 
     it 'is not valid when it does not have a start time' do
-      roll = Roll.new(title: 'Rick Roll', end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
+      roll = Roll.new(title: 'Bang', end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
       roll.valid?
       expect(roll.errors[:start_time]).to include("can't be blank")
     end
 
     it 'is not valid when it has a negative start time' do
-      roll = Roll.new(title: 'Rick Roll', start_time: -5, end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
+      roll = Roll.new(title: 'Bang', start_time: -5, end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
       roll.valid?
       expect(roll.errors[:start_time]).to include("must be greater than or equal to 0".)
     end
 
     it 'is not valid when it does not have an end time' do
-      roll = Roll.new(title: 'Rick Roll', start_time: 19, youtube_identifier: 'lYbYoaBrXpU')
+      roll = Roll.new(title: 'Bang', start_time: 19, youtube_identifier: 'lYbYoaBrXpU')
       roll.valid?
       expect(roll.errors[:end_time]).to include("can't be blank")
     end
 
     it 'is not valid when the end time is before the start time' do
-      roll = Roll.new(title: 'Rick Roll', start_time: 19, end_time: 10, youtube_identifier: 'lYbYoaBrXpU')
+      roll = Roll.new(title: 'Bang', start_time: 19, end_time: 10, youtube_identifier: 'lYbYoaBrXpU')
       #Error message will likely be set in custom validation -- can add here later
       expect(roll).to_not be_valid
     end
 
     it 'is not valid when it does not have a youtube uri' do
-      roll = Roll.new(title: 'Rick Roll', start_time: 19, end_time: 23)
+      roll = Roll.new(title: 'Bang', start_time: 19, end_time: 23)
       roll.valid?
       expect(roll.errors[:youtube_identifier]).to include("can't be blank")
     end
