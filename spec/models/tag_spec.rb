dd1c5b4e7c2d6b8a2a089a8c5efc6e604a972f1b
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-25 18:34:57 -0400

    add tag model specs for validity

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   it 'is valid with a name' do
-    tag = Tag.new(name: 'fail')
+    tag = Tag.new(name: 'animal fail')
     expect(tag).to be_valid
   end
 
@@ -16,5 +16,14 @@ RSpec.describe Tag, type: :model do
     tag = Tag.new(name: 'failure to launch')
     tag.valid?
     expect(tag.errors[:name]).to include("is too long (maximum is 16 characters)")
+  end
+
+  it 'is invalid if it contains characters other than lower-case letters and spaces' do
+    tag_with_capital_name = Tag.new(name: 'Fail')
+    tag_with_special_characters = Tag.new(name: 'f4il!')
+
+    #Error message will likely be set in custom validation -- can add here later
+    expect(tag_with_capital_name).to_not be_valid
+    expect(tag_with_special_characters).to_not be_valid
   end
 end
