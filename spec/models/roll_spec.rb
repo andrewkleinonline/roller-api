a27b193c850ee6a4525f13f3e009c884231b649f
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-25 17:53:06 -0400

    change expected error message for negative roll start time

--- spec/models/roll_spec.rb
+++ spec/models/roll_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe Roll, type: :model do
     it 'is not valid when it has a negative start time' do
       roll = Roll.new(title: 'Rick Roll', start_time: -5, end_time: 23, youtube_identifier: 'lYbYoaBrXpU')
       roll.valid?
-      expect(roll.errors[:start_time]).to include("can't be blank")
+      expect(roll.errors[:start_time]).to include("must be greater than or equal to 0".)
     end
 
     it 'is not valid when it does not have an end time' do
