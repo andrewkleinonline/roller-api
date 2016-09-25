04a90130cb0ca72ef099eacaaaa99d10939a429c
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-25 17:36:09 -0400

    add test for validity of roll with title, start time, end time, and youtube_uri

--- spec/models/roll_spec.rb
+++ spec/models/roll_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Roll, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validity:" do
+    it 'is valid when it has a title, start time, end time, and youtube uri' do
+      roll = Roll.new(title: 'Rick Roll', start_time: 19, end_time: 23, youtube_uri: 'lYbYoaBrXpU')
+      expect(roll).to be_valid
+    end
+  end
+
 end
