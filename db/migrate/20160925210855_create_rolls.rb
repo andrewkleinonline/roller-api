aa670c5f382ee36d35efe4f9ffe2cdab2e1607fd
Andrew Klein <andrew.klein.j@gmail.com>
2016-10-03 18:52:07 -0400

    add attributes to roll model

--- db/migrate/20160925210855_create_rolls.rb
+++ db/migrate/20160925210855_create_rolls.rb
@@ -1,6 +1,10 @@
 class CreateRolls < ActiveRecord::Migration[5.0]
   def change
     create_table :rolls do |t|
+      t.string :title
+      t.integer :start_time
+      t.integer :end_time
+      t.string :youtube_identifier
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,12 @@ ActiveRecord::Schema.define(version: 20160925221903) do
   enable_extension "plpgsql"
 
   create_table "rolls", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.integer  "start_time"
+    t.integer  "end_time"
+    t.string   "youtube_identifier"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "tags", force: :cascade do |t|
