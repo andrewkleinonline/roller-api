a2b2079f1d2ca4249147ddf81da243e9eebb12d0
Andrew Klein <andrew.klein.j@gmail.com>
2016-10-03 18:59:08 -0400

    add validation for start time numericality

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,4 +1,5 @@
 class Roll < ApplicationRecord
   validates :title, :start_time, :end_time, :youtube_identifier, presence: true
   validates :title, length: { maximum: 50 }
+  validates :start_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
