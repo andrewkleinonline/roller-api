63ffaf317582d26128586c339c6bf73228d76c41
Andrew Klein <andrew.klein.j@gmail.com>
2016-10-03 19:09:48 -0400

    add all validations to roll model

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -2,4 +2,21 @@ class Roll < ApplicationRecord
   validates :title, :start_time, :end_time, :youtube_identifier, presence: true
   validates :title, length: { maximum: 50 }
   validates :start_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :end_time_greater_than_start_time
+  validate :youtube_identifier_format
+
+  def end_time_greater_than_start_time
+    if end_time.to_i <= start_time.to_i
+      errors.add(:end_time, "Can't be less than start time." )
+    end
+  end
+
+  def youtube_identifier_format
+    if youtube_identifier
+      if !youtube_identifier.match(/\A[\w-]{11}\z/)
+        errors.add(:youtube_identifier, "Has wrong format")
+      end
+    end
+  end
+
 end
